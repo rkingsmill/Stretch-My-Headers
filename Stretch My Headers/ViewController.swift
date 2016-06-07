f25936a2fc62e1bf062fd89427c39927f441d761
RKing <rosalyn.kingsmill@gmail.com>
2016-06-07 12:01:16 -0400

    Initial Commit

---
+++ Stretch My Headers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretch My Headers
+//
+//  Created by Rosalyn Kingsmill on 2016-06-07.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Stretch My HeadersTests/Stretch_My_HeadersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Stretch_My_HeadersTests.swift
+//  Stretch My HeadersTests
+//
+//  Created by Rosalyn Kingsmill on 2016-06-07.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+import XCTest
+@testable import Stretch_My_Headers
+
+class Stretch_My_HeadersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
